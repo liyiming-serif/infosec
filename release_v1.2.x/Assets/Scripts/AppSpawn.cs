017365699b4e41c9da025d092407f8f657a45346
alexwzk <alexanderwzk@gmail.com>
2016-07-28 23:40:42 -0400

    add new scripts

---
+++ release_v1.2.x/Assets/Scripts/AppSpawn.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class AppSpawn : MonoBehaviour
+{
+
+    [SerializeField]
+    Windows appPrefab;
+
+    TaskManager manager;
+    int id;
+
+    bool one_click;
+    bool timer_running;
+    float timer_for_double_click;
+
+    //this is how long in seconds to allow for a double click
+    float delay = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        manager = GameObject.FindObjectOfType<TaskManager>();
+        Assert.IsNotNull(manager);
+        id = 0;
+        timer_for_double_click = Time.time;
+        one_click = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!one_click) // first click no previous clicks
+            {
+                one_click = true;
+
+                timer_for_double_click = Time.time; // save the current time
+                                                    // do one click things;
+            }
+            else
+            {
+                one_click = false; // found a double click, now reset
+                id = appPrefab.GetHashCode();
+                appPrefab.Register(id);
+                manager.AddNewTask(appPrefab);
+                Debug.Log("added");
+            }
+        }
+
+        if (one_click)
+        {
+            // if the time now is delay seconds more than when the first click started. 
+            if ((Time.time - timer_for_double_click) > delay)
+            {
+
+                //basically if thats true its been too long and we want to reset so the next click is simply a single click and not a double click.
+                one_click = false;
+
+            }
+        }
+    }
+}
---
+++ release_v1.2.x/Assets/Scripts/Windows.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class Windows : MonoBehaviour
+{
+
+    // Components 
+    Button minise;
+    Button close;
+    TaskManager manager;
+    public int id;
+
+    // This must be called before calling other methods
+    public void Register(int id)
+    {
+        this.id = id;
+
+    }
+
+    public bool IsRegistered()
+    {
+        return id != 0;
+    }
+
+    void SetSelfVisible(bool isVisible)
+    {
+        gameObject.SetActive(isVisible);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        id = 0;
+        manager = GameObject.FindObjectOfType<TaskManager>();
+        foreach (Button b in this.GetComponentsInChildren<Button>())
+        {
+            if (b.transform.name == "Minimise")
+            {
+                minise = b;
+            }
+            else if (b.transform.name == "Close")
+            {
+                close = b;
+            }
+        }
+        Assert.IsNotNull(manager);
+        Assert.IsNotNull(minise);
+        Assert.IsNotNull(close);
+
+        minise.onClick.AddListener(delegate
+        {
+            SetSelfVisible(false);
+            object[] values = new object[2] {id, false};
+            manager.SendMessage("SetBarItemVisible", values);
+        });
+        close.onClick.AddListener(delegate { Destroy(gameObject); manager.SendMessage("KillTask", id); });
+    }
+
+}
