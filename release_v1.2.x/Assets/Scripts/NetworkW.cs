f887e13af1cde529e25e997f90f2c3830742e22a
alexwzk <alexanderwzk@gmail.com>
2016-08-10 22:36:50 -0400

    minors

---
+++ release_v1.2.x/Assets/Scripts/NetworkW.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkW : GUI, IHasTitle {
+
+    public AlienController alienC;
+    public ServersGraphController serversC;
+    public List<Domain> urlString;
+
+    [SerializeField]
+    Animator alienA;
+    [SerializeField]
+    GameObject urlSPanel;
+    [SerializeField]
+    List<Domain> dnames;
+    
+    public string GetTitle()
+    {
+        return "Network";
+    }
+
+    protected void Awake()
+    {
+        base.Awake();
+        this.Register(this.GetHashCode());
+        urlString = new List<Domain>();
+        serversC = this.GetComponentInChildren<ServersGraphController>();
+    }
+
+    private void Start()
+    {
+        alienC =  alienA.GetComponent<AlienController>();
+    }
+
+    public void SendVictimTo(List<Domain> url)
+    {
+        if(!url[0])
+        {
+            ReturnTaskManager().appSpawn.animator.Rebind();
+            return;
+        }
+
+        foreach(Domain dname in url)
+        {
+            Object dclone = Instantiate(dname, urlSPanel.transform); // deep clone to network
+            urlString.Add((Domain) dclone);
+        }
+
+        urlString[0].GetComponent<Image>().color = Color.green;
+        StartCoroutine(FindThePath(1));
+    }
+
+    IEnumerator FindThePath(int id)
+    {
+        yield return new WaitForSeconds(1);
+        dnames[0].GetComponent<Image>().color = Color.green;
+        serversC.ToNextServer(1);
+    }
+    IEnumerator Done()
+    {
+        yield return new WaitForSeconds(0.5f);
+        alienA.gameObject.SetActive(false);
+    }
+    public void Explode()
+    {
+        GameObject o = Instantiate(Resources.Load("Explosion"), alienA.transform) as GameObject;
+        o.transform.localPosition = new Vector2(-50, 35);
+        StartCoroutine(Done());
+    }
+    public void Result()
+    {
+        serversC.ArriveNextServer();
+    }
+}
---
+++ release_v1.2.x/Assets/Scripts/ServersGraphController.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ServersGraphController : MonoBehaviour {
+
+    [SerializeField]
+    List<Image> dnames;
+    [SerializeField]
+    List<Animator> servers;
+    [SerializeField]
+    List<Animator> outpaths;
+    [SerializeField]
+    List<Animator> inpaths;
+
+    int counter;
+    List<bool> isBlinking;
+    int currentID;
+    int nextID;
+    int animationState;
+
+    void SetActive(Animator a, bool b)
+    {
+        a.enabled = b;
+    }
+
+    void SetBlink(int index, bool b)
+    {
+        isBlinking[index] = b;
+    }
+
+    IEnumerator StartWalking()
+    {
+        yield return new WaitForSeconds(1);
+        GetComponentInParent<NetworkW>().alienC.SetEndPosition(new Vector2(-292, 140));
+    }
+
+    IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(1);
+        GetComponentInParent<NetworkW>().Explode();
+    }
+    public void ToNextServer(int id)
+    {
+        animationState = 1;
+        SetBlink(currentID, false);
+        outpaths[currentID].GetComponent<SpriteRenderer>().enabled = true;
+        SetActive(outpaths[currentID], true);
+        SetActive(inpaths[id-1], true);
+        nextID = id;
+        StartCoroutine(StartWalking());
+    }
+
+    public void ArriveNextServer()
+    {
+        animationState = 0;
+        SetActive(outpaths[currentID], false);
+        SetActive(inpaths[nextID - 1], false);
+        SetActive(servers[nextID], true);
+        currentID = nextID;
+        StartCoroutine(Explode());
+    }
+
+    // Use this for initialization
+    void Start () {
+	    foreach (Animator a in servers)
+        {
+            SetActive(a, false);
+        }
+        foreach (Animator a in inpaths)
+        {
+            SetActive(a, false);
+        }
+        foreach(Animator a in outpaths)
+        {
+            SetActive(a, false);
+        }
+        SetActive(servers[0], true);
+        isBlinking = new List<bool>();
+        isBlinking.Add(true);
+        for(int i = 1; i < outpaths.Count; i++)
+        {
+            isBlinking.Add(false);
+        }
+        counter = 0;
+        currentID = 0;
+        animationState = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(counter % 50 == 0)
+        {
+            for(int i = 0; i < isBlinking.Count; i++)
+            {
+                if(isBlinking[i])
+                {
+                    SpriteRenderer o = outpaths[i].GetComponent<SpriteRenderer>();
+                    o.enabled = !o.enabled;
+                }
+            }
+            counter = 1;
+        }
+        else
+        {
+            counter++;
+        }
+	}
+
+
+}
