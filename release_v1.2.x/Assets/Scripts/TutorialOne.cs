ef3d2837af87b8538ae1afe13f18e817d3f6ca67
alexwzk <alexanderwzk@gmail.com>
2016-08-15 01:07:24 -0400

    add tutorials

---
+++ release_v1.2.x/Assets/Scripts/TutorialOne.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialOne : MonoBehaviour {
+
+	void Start () {
+        if (NetworkWindows.instance.gameObject.activeSelf)
+        {
+            NetworkWindows.instance.SetSelfVisible(false);
+        }
+	}
+}
---
+++ release_v1.2.x/Assets/Scripts/TutorialTwo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialTwo : MonoBehaviour {
+
+    Slot s;
+	// Use this for initialization
+	void Start () {
+        s = GetComponentInChildren<Slot>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (s.holding)
+        {
+            NetworkWindows.instance.SetSelfVisible(true);
+        }
+	}
+}
